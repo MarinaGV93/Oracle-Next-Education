7e1a638947645115f4b3638a7ff8562f3c8e1379
MarinaGV93 <marinasgv@hotmail.com>
2024-02-02 18:00:17 -0300

    Desafio listas

---
+++ Lógica de programação explore funções e listas/Desafios/Listas/app.js
@@ -0,0 +1,23 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaDeNomes = ["Marta", "Júnior", "Joana"];
+
+console.log(listaDeNomes[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let listaFrutas = ["Banana", "Maçã", "Manga"];
+
+console.log(listaFrutas[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let listaNumero = ["1", "2", "3"];
+
+console.log(listaNumero[2]);
